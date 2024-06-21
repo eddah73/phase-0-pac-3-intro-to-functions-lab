242426f6c4d26006f3ea6f27787c27a73b0c9d49
eddah chepkoech <eddahsjr@gmail.com>
2024-06-22 02:36:25 +0300

    return,log,selection condition

---
+++ index.js
@@ -0,0 +1,42 @@
+function  shout(string){
+    return string.toUpperCase();
+
+}
+
+function  whisper(string){
+    return string.toLowerCase();
+
+}
+function logShout(string){
+    console.log(string.toUpperCase());
+
+}
+// logShout();
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+
+}
+// logWhisper();
+ function sayHiToHeadphonedRoommate(string){
+    return string.toLowerCase();
+    console.log( "I can't hear you!");
+
+ }
+
+// let string="hello";
+
+function sayHiToHeadphonedRoommate( string){
+  if(string === 'hello')
+    // console.log("I can't hear you!")
+    return"I can't hear you!";
+
+else if(string === 'HELLO')
+
+     return "YES INDEED!";
+else(string ==="Let's have dinner together!")
+  return"I would love to!";
+
+}
+// sayHiToHeadphonedRoommate();
+
